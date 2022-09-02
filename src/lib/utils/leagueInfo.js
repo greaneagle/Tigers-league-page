8adb6862779246e37adc868216f56199f69f591c
grexon1717 <112695097+grexon1717@users.noreply.github.com>
2022-09-02 14:55:22 +0200

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -134,7 +134,7 @@ export const managers = [
        "roster": 6,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
        "name": "Matija Klančar",
        "tookOver": 2018, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-       "location": "Kranj, Slovenia", // (optional)
+       "location": "Domžale, Slovenia", // (optional)
        "bio": "A lot of the unknown in the known.",
        "photo": "/managers/matija.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
@@ -222,22 +222,22 @@ export const managers = [
        "roster": 10,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
        "name": "Andrej Kuhelj",
        "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
-       "location": "Kranj, Slovenia", // (optional)
-       "bio": "Lorem ipsum...",
+       "location": "Gorjuša, Slovenia", // (optional)
+       "bio": "100% get emotionally attached to my squad",
        "photo": "/managers/andrej.jpg", // square ratio recommended (no larger than 500x500)
        "fantasyStart": 2016, // (optional) when did the manager start playing fantasy football
-       "favoriteTeam": "kc", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+       "favoriteTeam": "arz", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+       "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
        "rival": {
-         name: "BS3 Mafija", // Can be anything (usually your rival's name)
-         link: null, // manager array number within this array, or null to link back to all managers page
-         image: "/managers/rival.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+         name: "Rok Gregorič", // Can be anything (usually your rival's name)
+         link: 7, // manager array number within this array, or null to link back to all managers page
+         image: "/managers/gregoric.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
-       "favoritePlayer": 1426, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+       "favoritePlayer": 1576, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
        "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
        "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-       "philosophy": "Your fantasy team's philosophy",
-       "tradingScale": 10, // 1 - 10
+       "philosophy": "Get points now, retire later!",
+       "tradingScale": 8, // 1 - 10
        "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
      }
   ]
