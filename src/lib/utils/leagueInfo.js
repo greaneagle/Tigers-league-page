1ac2b6ad1a21a17110fcb37f907d27387798856e
grexon1717 <112695097+grexon1717@users.noreply.github.com>
2022-09-02 14:57:40 +0200

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -142,7 +142,7 @@ export const managers = [
        "mode": "Dynasty", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
        "rival": {
          name: "Miha Tivadar", // Can be anything (usually your rival's name)
-         link: 4, // manager array number within this array, or null to link back to all managers page
+         link: 3, // manager array number within this array, or null to link back to all managers page
          image: "/managers/tivadar.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
        "favoritePlayer": 421, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
@@ -230,7 +230,7 @@ export const managers = [
        "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
        "rival": {
          name: "Rok Gregorič", // Can be anything (usually your rival's name)
-         link: 7, // manager array number within this array, or null to link back to all managers page
+         link: 6, // manager array number within this array, or null to link back to all managers page
          image: "/managers/gregoric.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
        "favoritePlayer": 1576, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
