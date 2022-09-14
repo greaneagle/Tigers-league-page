f8022eca446a1e1432f9efa8ce8588e6f6722b38
grexon1717 <112695097+grexon1717@users.noreply.github.com>
2022-09-14 15:30:30 +0200

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -44,9 +44,9 @@ export const managers = [
        "favoriteTeam": "min", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
        "rival": {
-         name: "Klemen Svetlin", // Can be anything (usually your rival's name)
-         link: 2, // manager array number within this array, or null to link back to all managers page
-         image: "/managers/svetlin.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+         name: "Urban Repanšek", // Can be anything (usually your rival's name)
+         link: 8, // manager array number within this array, or null to link back to all managers page
+         image: "/managers/urban.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
        "favoritePlayer": 1213, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
        "valuePosition": "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
@@ -66,9 +66,9 @@ export const managers = [
        "favoriteTeam": "kc", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
        "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
        "rival": {
-         name: "Rok Gregorič", // Can be anything (usually your rival's name)
-         link: 6, // manager array number within this array, or null to link back to all managers page
-         image: "/managers/gregoric.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+         name: "Klemen Svetlin", // Can be anything (usually your rival's name)
+         link: 2, // manager array number within this array, or null to link back to all managers page
+         image: "/managers/svetlin.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
        "favoritePlayer": 1466, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
        "valuePosition": "QB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
